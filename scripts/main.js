e18890c5f521f61319c6eafec9db4956fdb8ad23
Roxy <erippley@gmail.com>
2020-09-02 21:06:09 +0200

    lager Code für Todoliste und Taschenrechner in eigene Dateien aus

--- scripts/main.js
+++ scripts/main.js
@@ -49,100 +49,12 @@ if (!localStorage.getItem('name')) {
 myButton.onclick = function () {
     setUserName();
 }
+let calculator = new Calculator();
+calculator.mathProblem();
 
-function calculater() {
+let todo = new Todo();
+todo.zeichneListe();
 
-    let randomNumberOne = Math.floor(Math.random() * 10 + 1); //random gibt uns nur Zahlen zwischen 0 und 1. Um Zahlen zwischen 0 und 9 zu bekommen, muss mit 10 multipliziert werden, +1 sorgt dafür, dass auch die 10 genutzt werden kann. floor() ignoriert die Nachkommastellen
-    let randomNumberTwo = Math.floor(Math.random() * 10 + 1);
-    let rechnung = document.createElement("div");
-
-    rechnung.id = "calculater";
-    //rechnung.setAttribute("id", "calculater"); -> alternative auch für andere Attribute
-    rechnung.textContent = "Calculate: " + randomNumberOne + " + " + randomNumberTwo + " = " + "?";
-    document.querySelector("#calculaterIntroduction").after(rechnung);
-
-}
-
-calculater();
-
-let resultButton = document.querySelector("#result");
-resultButton.onclick = function () {
-    setResult();
-}
-
-function setResult() {
-    let yourResult = prompt("Write your result!");
-    resultOfCalculater(yourResult);
-
-}
-
-function resultOfCalculater(yourResult) {
-    let result = document.createElement("div");
-    result.id = "yourResult";
-    result.textContent = "Your result: " + yourResult;
-    document.querySelector("#calculater").after(result);
-}
-
-
-let toDoButton = document.querySelector("#whatDoYouWantToDo");
-let toDoList = document.querySelector("#toDo");
-let liste = [];
-
-function setToDo() {
-    let myToDo = prompt("Write your todo.");//promt() öffnet eine Dialogbox wo etwas eingegeben werden kann
-
-    if (myToDo !== "" && myToDo !== null) {
-        liste.push(myToDo);
-        localStorage.setItem("toDoList", JSON.stringify(liste));
-        let newPoint = document.createElement("li");
-        toDoList.appendChild(newPoint);
-        newPoint.textContent = myToDo;
-    }
-}
-
-//let x = function(){}; function setToDo (){}
-//Damit der Button geht
-toDoButton.onclick = function () { // toDoButton.onclick = setToDo;
-    setToDo();
-}
-
-//Damit die Liste angezeigt wird nachdem die Seite neu aufgerufen wurde
-function zeichneListe() {
-    let doit = localStorage.getItem("toDoList");
-    if (doit != null) {
-        liste = JSON.parse(doit);
-        for (let i = 0; i < liste.length; i++) {
-            let newp = document.createElement("li");
-            toDoList.appendChild(newp);
-            newp.textContent = liste[i];
-        }
-    }
-}
-
-zeichneListe();
-
-let deleteButton = document.querySelector("#delete");
-
-deleteButton.onclick = function () {
-    deleteTodo();
-}
-
-function deleteTodo() {
-    let numberOfTodo = prompt("Which todo should be deleted?");
-    let intIndex = parseInt(numberOfTodo, 10);//Wenn die letzte Zahl weg gelassen wird, wird automatisch das 10er-System genutzt
-    let deleteIndex = (intIndex - 1);
-    if (!isNaN(deleteIndex)) {
-        liste.splice(deleteIndex, 1);
-        localStorage.setItem("toDoList", JSON.stringify(liste));
-        let listItemIndex = toDoList.childNodes.item(deleteIndex);
-        toDoList.removeChild(listItemIndex);
-        let allToDo = document.getElementById("toDo");
-        while (allToDo.firstChild) {
-            allToDo.removeChild(allToDo.lastChild);
-        }
-        zeichneListe();
-    }
-}
 
 window.addEventListener("mousemove", function (event) {
 
